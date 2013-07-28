0e4c6358ba3a3378dc85aa4bb049434984f00f68
Michael Sanchez <michael.h.sanchez@gmail.com>
2013-07-28 09:33:04 -0400

    Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Omrails::Application.routes.draw do
+  root :to => 'pages#home'
+  get 'howitworks' => 'pages#howitworks'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,8 +50,7 @@ Omrails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
